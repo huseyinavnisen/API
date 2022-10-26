8e0b7c78c49a7fd0fd6f1145ce8ff64c008f2c39
huseyinavnisen <havnisen@gmail.com>
2022-10-26 21:12:19 +0300

    26.10.2022

--- src/test/java/Get03.java
+++ src/test/java/Get03.java
@@ -1,9 +1,11 @@
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-public class Get03 extends JsonplaceholderBaseUrl{
+public class Get03 extends JsonplaceholderBaseUrl {
      /*
       Given
           https://jsonplaceholder.typicode.com/todos/23
@@ -23,16 +25,35 @@ public class Get03 extends JsonplaceholderBaseUrl{
    */
 
     @Test
-    public void get01(){
+    public void get01() {
         // set url
-        spec.pathParams("first","todos","second",23);
+        spec.pathParams("first", "todos", "second", 23);
 
         // Expected Data
 
         // Send the request and Get Response
 
-        Response response=given().spec(spec).when().get("/{first}/{second}");
+        Response response = given().spec(spec).when().get("/{first}/{second}");
         response.prettyPrint();
+
+        // Do assertion
+        // 1.Yol - Hard Assert
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType("application/json").
+                body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit")).
+                body("completed",equalTo(false)).
+                body("userId",equalTo(2));
+
+        // 2.Yol - Soft Assert ===> sadece BODY içinde geçerlidir
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit"),
+                "completed",equalTo(false),"userId",equalTo(2));
+
 
 
 
